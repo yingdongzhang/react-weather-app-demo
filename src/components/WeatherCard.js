099612bb934a22c10314a3f02fb130b95a420033
Yingdong Zhang <zhangyingdong89@gmail.com>
2020-10-08 20:33:47 +1100

    demo using local state

---
+++ src/components/WeatherCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Input,
+  Card,
+  Image,
+} from 'semantic-ui-react'
+import './WeatherCard.css'
+
+export default function WeatherCard({city, weather}){
+  const { icon, temp, feels_like, temp_min, temp_max, description } = weather
+  return (
+    <Card>
+      <Image src={`https://openweathermap.org/img/wn/${icon}@4x.png`} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{`${city}. Now: ${temp}°C`}</Card.Header>
+        <Card.Meta><span>{`Feels like ${feels_like}°C, min ${temp_min}°C, max ${temp_max}°C`}</span></Card.Meta>
+        <Card.Description>{description}</Card.Description>
+      </Card.Content>
+    </Card>
+  )
+}
