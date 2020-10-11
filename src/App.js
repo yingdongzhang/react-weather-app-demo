795b0b1bd4f5869201c6935d0cb2ebbbed60cd28
Yingdong Zhang <zhangyingdong89@gmail.com>
2020-10-11 18:50:13 +1100

    feat: create a starting point for demo purpose

--- src/App.js
+++ src/App.js
@@ -1,93 +1,28 @@
 import React from 'react'
 import { Form, Input, Button } from 'semantic-ui-react'
-import moment from 'moment'
 
 import './App.css'
-import WeatherCard from './components/WeatherCard'
-import fetchWeatherApi from './api/fetchWeatherApi'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    
-    this.state = {
-      city: '',
-      weather: null,
-      loading: false,
-      time: '',
-      intervId: null,
-    }
-  }
-
-  handleCityInputChange = (e) => {
-    this.setState({
-      city: e.target.value,
-      time: '',
-      weather: null,
-    })
-  }
-
-  fetchWeather = async () => {
-    this.setState({
-      loading: true,
-    })
-
-    const weather = await fetchWeatherApi(this.state.city)
-
-    this.setState({
-      weather,
-      loading: false,
-    })
-  }
-
-  componentDidUpdate() {
-    const { city, weather } = this.state
-    if (weather) {
-      document.title = `${city} - ${weather.temp}°C`
-    }
-  }
-
-  componentDidMount() {
-    const intervId = setInterval(this.getCurrentTime, 1000)
-    this.setState({
-      intervId,
-    })
-  }
-
-  componentWillUnmount() {
-    console.log('componentWillUnmount, clear interval -> ', this.state.intervId)
-    clearInterval(this.state.intervId)
-  }
-
-  getCurrentTime = () => {
-    const { city, weather } = this.state
-    if (city && weather) {
-      const localTime = moment.utc(moment().utc().valueOf() + weather.timezone*1000)
-      this.setState({
-        time: localTime.format('DD/MM/YYYY HH:mm:ss')
-      })
-    }
-  }
-
   render() {
-    const { city, time, weather, loading } = this.state
     return (
       <div>
+        <div className="row">
+          <h1>Weather Card</h1>
+        </div>
         <div className="row">
           <Form>
             <Form.Group inline>
               <Form.Field>
                 <label>Enter your city</label>
-                <Input placeholder='Melbourne, AU' loading={loading} onChange={this.handleCityInputChange} />
+                <Input placeholder='Melbourne, AU' />
               </Form.Field>
-              <Button primary loading={loading} onClick={this.fetchWeather}>Search</Button>
+              <Button primary>Search</Button>
             </Form.Group>
           </Form>
         </div>
         <div className="row">
-          <h2>Current weather in {city}</h2>
-          {weather && <WeatherCard weather={weather} />}
-          {time && <h3>Local time {time}</h3>}
+
         </div>
       </div>
     )
--- src/AppWithHooks.js
+++ src/AppWithHooks.js
@@ -1,32 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Form, Input, Button } from 'semantic-ui-react'
-import moment from 'moment'
 
 import './App.css'
-import WeatherCard from './components/WeatherCard'
-import fetchWeatherApi from './api/fetchWeatherApi'
 
 export default function AppWithHooks() {
-  const [ city, setCity ] = useState('')
-  const [ weather, setWeather ] = useState(null)
-  const [ loading, setLoading ] = useState(false)
-
-  useDocumenTitle(city, weather)
-  const [ time, setTime ] = useLocalTime(city, weather)
-
-  function handleCityInputChange(e) {
-    setCity(e.target.value)
-    setTime('')
-    setWeather(null)
-  }
-
-  async function fetchWeather() {
-    setLoading(true)
-    const weather = await fetchWeatherApi(city)
-    setWeather(weather)
-    setLoading(false)
-  }
-
   return (
     <div>
       <div className="row">
@@ -34,44 +11,15 @@ export default function AppWithHooks() {
           <Form.Group inline>
             <Form.Field>
               <label>Enter your city</label>
-              <Input placeholder='Melbourne, AU' loading={loading} onChange={handleCityInputChange} />
+              <Input placeholder='Melbourne, AU' />
             </Form.Field>
-            <Button primary loading={loading} onClick={fetchWeather}>Search</Button>
+            <Button primary>Search</Button>
           </Form.Group>
         </Form>
       </div>
       <div className="row">
-        <h2>Current weather in {city}</h2>
-        {weather && <WeatherCard weather={weather} />}
-        {time && <h3>Local time {time}</h3>}
+
       </div>
     </div>
   )
-}
-
-function useDocumenTitle(city, weather) {
-  useEffect(() => {
-    if (weather) {
-      document.title = `${city} - ${weather.temp}°C`
-    }
-  })
-}
-
-function useLocalTime(city, weather) {
-  const [ time, setTime ] = useState('')
-
-  useEffect(() => {
-    const intervId = setInterval(() => {
-      if (city && weather) {
-        const localTime = moment.utc(moment().utc().valueOf() + weather.timezone*1000)
-        setTime(localTime.format('DD/MM/YYYY HH:mm:ss'))
-      }
-    }, 1000)
-    return () => {
-      console.log('useLocalTime effect, clear interval -> ', intervId)
-      clearInterval(intervId)
-    }
-  })
-
-  return [ time, setTime ]
 }
