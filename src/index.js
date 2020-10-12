80733438126378e1352a37456596836d1eb72738
Yingdong Zhang <zhangyingdong89@gmail.com>
2020-10-12 22:00:40 +1100

    chore: add snippet for testing component unmounts and re-mounts

--- src/index.js
+++ src/index.js
@@ -16,6 +16,21 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
+// setInterval(() => {
+//   if (Date.now() %2 === 0) {
+//     ReactDOM.render(
+//         <App />,
+//         // <AppWithHooks />,
+//       document.getElementById('root')
+//     )
+//   } else {
+//     ReactDOM.render(
+//         <h1>hello</h1>,
+//       document.getElementById('root')
+//     )
+//   }
+// }, 1000)
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
